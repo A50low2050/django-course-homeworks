c2622bbf971a1cf26ec73ab7fc6a60b597f26522
Artem <artem.alshan@yandex.ru>
2024-02-14 15:17:40 +0300

    Relations many-to-many

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Tag, Scope
 
-from .models import Article
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            tag = form.cleaned_data['is_main']
+            if tag is False:
+                raise ValidationError('Укажите основной тэг')
+            return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 1
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
